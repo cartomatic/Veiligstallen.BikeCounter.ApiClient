0a9c674b1ea6f83c4ee9fe17f419af89ddde4e95
Dominik Mikiewicz <dominik@cartomatic.pl>
2022-11-05 15:23:44 +0100

    survey areas downloadable

--- Veiligstallen.BikeCounter.ApiClient/Service/SurveyArea.cs
+++ Veiligstallen.BikeCounter.ApiClient/Service/SurveyArea.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using Remotion.Linq.Parsing.Structure.IntermediateModel;
 using RestSharp;
 using Veiligstallen.BikeCounter.ApiClient.DataModel;
 
@@ -48,5 +50,75 @@ namespace Veiligstallen.BikeCounter.ApiClient
         /// <returns></returns>
         public Task DeleteSurveyAreaAsync(string surveyAreaId)
             => DeleteObjectAsync(new RequestConfig(Configuration.Routes.SURVEY_AREA, surveyAreaId));
+
+
+        /// <summary>
+        /// Prepares survey data for download
+        /// </summary>
+        /// <param name="queryParams"></param>
+        /// <param name="outDir"></param>
+        /// <returns></returns>
+        public async Task<Guid> PrepareSurveyAreasDownloadAsync(Dictionary<string, string> queryParams, string outDir)
+        {
+            queryParams ??= new Dictionary<string, string>();
+
+            //reset paging
+            queryParams["start"] = "0";
+            queryParams["limit"] = "100000";
+
+            var (data, _) = await GetObjectsAsync<SurveyArea>(new RequestConfig(Configuration.Routes.SURVEY_AREAS) { QueryParams = queryParams });
+
+            var downloadId = Guid.NewGuid();
+
+            //serialize
+            using var fs = System.IO.File.OpenWrite(Path.Combine(outDir, $"{downloadId}"));
+            using var sw = new StreamWriter(fs);
+
+            //hdr
+            await sw.WriteAsync(
+                string.Join("\t", new []
+                {
+                    nameof(SurveyArea.Id),
+                    nameof(SurveyArea.LocalId),
+                    nameof(SurveyArea.ParentLocalId),
+                    nameof(SurveyArea.Name),
+                    nameof(SurveyArea.Parent),
+                    nameof(SurveyArea.ValidFrom),
+                    nameof(SurveyArea.ValidThrough),
+                    nameof(SurveyArea.Authority),
+                    nameof(SurveyArea.XtraInfo),
+                    nameof(SurveyArea.SurveyAreaType),
+                    nameof(SurveyArea.GeoLocation),
+                    Environment.NewLine
+                })
+            );
+
+            //content
+            foreach (var surveyArea in data)
+            {
+                await sw.WriteAsync(
+                    string.Join("\t", new[]
+                    {
+                        surveyArea.Id,
+                        surveyArea.LocalId,
+                        surveyArea.ParentLocalId,
+                        surveyArea.Name,
+                        surveyArea.Parent,
+                        surveyArea.ValidFrom?.ToString("O"),
+                        surveyArea.ValidThrough?.ToString("O"),
+                        surveyArea.Authority,
+                        surveyArea.XtraInfo,
+                        surveyArea.SurveyAreaType,
+                        $"{_cfg.Endpoint}/{Configuration.Routes.SURVEY_AREAS}/{surveyArea.Id}",
+                        Environment.NewLine
+                    })
+                );
+            }
+
+            await sw.FlushAsync();
+            sw.Close();
+
+            return downloadId;
+        }
     }
 }
